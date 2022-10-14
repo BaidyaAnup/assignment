c7a5838df838ffae2299f185cbd4eb119c59a24c
Anup Baidya <anupbaidya619@gmail.com>
2022-10-14 20:17:43 +0530

    project initialization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Orange.destroy_all
+Apple.destroy_all
+Basket.destroy_all
+
+b1 = Basket.create!(
+  :capacity => 27,
+  :fill_rate => 0
+)
+
+b2 = Basket.create!(
+  :capacity => 5,
+  :fill_rate => 0
+)
+
+b3 = Basket.create!(
+  :capacity => 5,
+  :fill_rate => 0
+)
+
+a1 = Apple.create!(
+  :basket => b1,
+  :variety => "Red"
+)
+
+a2 = Apple.create!(
+  :basket => b2,
+  :variety => "Green"
+)
+
+a3 = Apple.create!(
+  :basket => b3,
+  :variety => "Pink"
+)
+
+o1 = Orange.create!
